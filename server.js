3f3b70447910404ecb1d735d2d1ed6f4ff36512f
Jeetanand03 <jeetand1729@gmail.com>
2024-07-16 19:23:53 +0530

    user authentication step completed

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require("express")
+const app=express()
+const bcrypt= require("bcrypt")
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+})
+app.get("/login",(req,res)=>{
+    res.render("login.ejs")
+})
+app.get("/register",(req,res)=>{
+    res.render("register.ejs")
+})
+
+app.listen(3000)
